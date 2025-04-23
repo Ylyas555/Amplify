ea16796e85ddefc7ec7fa2867e53c7bf3de79dd9
Ylyas555 <ylyas_movlyamov@stu.indianhills.edu>
2025-04-23 14:46:16 -0500

    final

--- app/page.tsx
+++ app/page.tsx
@@ -26,9 +26,14 @@ export default function App() {
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
+    const content = window.prompt(`Todo content`);
+    if (content) {
+      client.models.Todo.create({ content });
+    }
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id });
   }
 
   return (
@@ -37,7 +42,13 @@ export default function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li
+            key={todo.id}
+            onClick={() => deleteTodo(todo.id)}
+            style={{ cursor: "pointer" }}
+          >
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
